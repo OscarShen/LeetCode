ad6e8e9d2ee8e6eb4c20d0d7fce15d942ee8cb15
ruiyao.shen <oscarshen09@foxmail.com>
2016-06-24 16:27:48 +0800

    I finish Best Time to Buy and Sell I II III today

--- src/bestTimeToBuyAndSellStockIII/BestTimeToBuyAndSellStockIII.java
+++ src/bestTimeToBuyAndSellStockIII/BestTimeToBuyAndSellStockIII.java
@@ -1,54 +1,32 @@
 package bestTimeToBuyAndSellStockIII;
 
 public class BestTimeToBuyAndSellStockIII {
-	private int first_start = 0;
-	private int first_end = 0;
-
 	public int maxProfit(int[] prices) {
-		int first = first_Profit(prices);
-		int second = second_Profit(prices);
-		return first + second;
-	}
+		int n = prices.length;
+		int[] preProfit = new int[n];
+		int[] postProfit = new int[n];
 
-	public int second_Profit(int[] prices) {
-		int[] left_prices = new int[first_start + 1];
-		System.arraycopy(prices, 0, left_prices, 0, first_start + 1);
-		int second_thisProfit1 = first_Profit(left_prices);
-
-		int[] right_prices = new int[prices.length - first_end + 1];
-		System.arraycopy(prices, first_end, right_prices, 0, prices.length
-				- first_end + 1);
-		int sendcond_thisProfit2 = first_Profit(right_prices);
-		return second_thisProfit1 > sendcond_thisProfit2 ? second_thisProfit1
-				: sendcond_thisProfit2;
-	}
-
-	public int first_Profit(int[] prices) {
-		int first_maxProfit = 0;
-		int first_thisProfit = 0;
-		int[] profits = new int[prices.length];
-
-		for (int i = 0; i < prices.length - 1; i++) {
-			profits[i] = prices[i + 1] + prices[i];
-			first_thisProfit += profits[i];
-			if (first_thisProfit > first_maxProfit) {
-				first_maxProfit = first_thisProfit;
-				first_end = i;
-			}
-			if (first_thisProfit < 0) {
-				first_thisProfit = 0;
-			}
+		if (n < 2) {
+			return 0;
 		}
 
-		first_thisProfit = 0;
-		for (int i = first_end; i >= 0; i--) {
-			first_thisProfit += profits[i];
-			if (first_thisProfit == first_maxProfit) {
-				first_start = i;
-				break;
-			}
+		int curMin = prices[0];
+		for (int i = 1; i < n; i++) {
+			curMin = Math.min(curMin, prices[i]);
+			preProfit[i] = Math.max(preProfit[i - 1], prices[i] - curMin);
 		}
 
-		return first_maxProfit;
+		int curMax = prices[n - 1];
+		for (int i = n - 2; i >= 0; i--) {
+			curMax = Math.max(curMax, prices[i]);
+			postProfit[i] = Math.max(postProfit[i + 1], curMax - prices[i]);
+		}
+
+		int maxProfit = 0;
+		for (int i = 0; i < n; i++) {
+			maxProfit = Math.max(maxProfit, preProfit[i] + postProfit[i]);
+		}
+
+		return maxProfit;
 	}
 }
--- src/bestTimeToBuyAndSellStockIII/test.java
+++
@@ -1,9 +0,0 @@
-package bestTimeToBuyAndSellStockIII;
-
-public class test {
-	public static void main(String[] args) {
-		int[] prices = { 4, 3, 2, 3, 4, 5, 6, 5, 3, 5, 7 };
-		System.out
-				.println(new BestTimeToBuyAndSellStockIII().maxProfit(prices));
-	}
-}
