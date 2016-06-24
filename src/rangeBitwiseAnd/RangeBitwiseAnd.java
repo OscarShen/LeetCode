077594991b48f2edd417c67d4c3298f9b293014c
ruiyao.shen <oscarshen09@foxmail.com>
2016-06-24 18:20:57 +0800

    Bitwise AND of Numbers Range

---
+++ src/rangeBitwiseAnd/RangeBitwiseAnd.java
@@ -0,0 +1,13 @@
+package rangeBitwiseAnd;
+
+public class RangeBitwiseAnd {
+	public int rangeBitwiseAnd(int m, int n) {
+		int i = 0;
+		while (m != n) {
+			m >>= 1;
+			n >>= 1;
+			i++;
+		}
+		return m << i;
+	}
+}
